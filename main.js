493053396fedaafd8fee0db4fcb4bd3ea9cc8397
fedPass <federica.passanante@gmail.com>
2019-11-29 16:12:30 +0100

    struttura html e style base

---
+++ main.js
@@ -0,0 +1,4 @@
+
+15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
+Suggerimento: usare una classe per individuare quali quadratini diventano rossi
+BONUS: generare la griglia dei quadratini utilizzando jQuery
