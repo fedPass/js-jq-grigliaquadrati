30fbe13462d24aab8ce8b05b5c29ecd111faca90
fedPass <federica.passanante@gmail.com>
2019-11-29 17:53:43 +0100

    aggiunta funzione random per generare bombe aggiungi classe active

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+$(document).ready(function(){
+    // genero 15 numeri che corrispondono all'indice dei quadrati che prenderanno la classe bomba
+    var indiciBombe = [2,5,8,3,6];
+    do {
+        var numero = generaRandom(0,63);
+        if (!indiciBombe.includes(numero)) {
+            indiciBombe.push(numero);
+        }
+    } while (indiciBombe.length < 15);
+    console.log('Gli indici delle bombe sono: ' + indiciBombe);
 
-15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
-Suggerimento: usare una classe per individuare quali quadratini diventano rossi
-BONUS: generare la griglia dei quadratini utilizzando jQuery
+    //prendo il quadrato con l'indice[indiciBombe] e gli aggiungo la classe bomba (assegnazione classe)
+    for (var i = 0; i < indiciBombe.length; i++) {
+        $('.quadrato').eq(indiciBombe[i]).addClass('bomba');
+    }
+
+    //se clicco su bomba aggiungi classe active
+    $('.quadrato').click(function(){
+        $(this).addClass('active');
+    });
+
+
+    function generaRandom(min, max) {
+        var random = Math.floor(Math.random() * (max - min + 1) ) + min;
+        return random;
+    }
+
+})
